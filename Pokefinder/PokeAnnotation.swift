38070aa847c176e757fd24d65e192db60d4e0d7c
MAC <mac@IrisdeMacBook-Pro.local>
2016-10-07 17:19:13 +0800

    first commit V1 random Pokemon

---
+++ Pokefinder/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  Pokefinder
+//
+//  Created by MAC on 07/10/2016.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+import Foundation
---
+++ Pokefinder/Pokefinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Pokefinder-Bridging-Header.h
+//  Pokefinder
+//
+//  Created by MAC on 06/10/2016.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+#ifndef Pokefinder_Bridging_Header_h
+#define Pokefinder_Bridging_Header_h
+
+
+#endif /* Pokefinder_Bridging_Header_h */
